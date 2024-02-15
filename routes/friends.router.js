8da2be23d5f1f6d349306d8d8901bd7174ed258e
BauteNonSinger <bautista19298@gmail.com>
2024-02-15 16:28:58 +0100

    routes

---
+++ routes/friends.router.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const friendsRouter = express.Router();
+const friendsController = require("../controllers/friends.controller");
+
+
+friendsRouter.post("/", friendsController.postFriend);
+friendsRouter.get("/", friendsController.getFriends);
+friendsRouter.get("/:friendId", friendsController.getOneFriend);
+
+
+module.exports = friendsRouter;
