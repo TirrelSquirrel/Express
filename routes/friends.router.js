66723680ba8f4d28e8a4c9c2567ce38d1f685ffd
BauteNonSinger <bautista19298@gmail.com>
2024-02-15 16:34:49 +0100

    callback in friends

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -3,7 +3,10 @@ const express = require('express')
 const friendsRouter = express.Router();
 const friendsController = require("../controllers/friends.controller");
 
-
+friendsRouter.use((req, res, next) => {
+    console.log('IP:', req.ip)
+    next();
+})
 friendsRouter.post("/", friendsController.postFriend);
 friendsRouter.get("/", friendsController.getFriends);
 friendsRouter.get("/:friendId", friendsController.getOneFriend);
